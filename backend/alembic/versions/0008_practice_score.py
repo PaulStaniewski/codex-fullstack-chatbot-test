c228ddd780feb9b1dcd0455ec207bda9683489c7
paulstaniewski <paulstaniewski@gmail.com>
2026-04-27 21:00:47 +0200

    Add advanced lesson catalog

--- backend/alembic/versions/0008_practice_score.py
+++ backend/alembic/versions/0008_practice_score.py
@@ -1,6 +1,6 @@
 """add practice submission score metadata
 
-Revision ID: 0008_add_practice_submission_score
+Revision ID: 0008_practice_score
 Revises: 0007_add_practice_submissions
 Create Date: 2026-04-27 00:00:00.000000
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 from alembic import op
 
 
-revision: str = "0008_add_practice_submission_score"
+revision: str = "0008_practice_score"
 down_revision: Union[str, None] = "0007_add_practice_submissions"
 branch_labels: Union[str, tuple[str, ...], None] = None
 depends_on: Union[str, tuple[str, ...], None] = None
